effc7cb62f345b4e6ec895d992bb389b88a70404
shyqulreyad <shyqulislam@gmail.com>
2023-05-20 16:45:46 +0600

    multiply every value with gap of two

--- Numpy_intro/intro.py
+++ Numpy_intro/intro.py
@@ -4,3 +4,8 @@ print(list(range(0,10,2)))
 list1 = [1,2,3,4,5]
 list2 = [6,7,8,9,10]
 # print(list1 * list2)
+
+new_list = []
+for i in range(0,len(list1),2):
+    new_list.append(list1[i] * list2[i])
+print(new_list)
