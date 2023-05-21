4333c596cb9e447e3b5b9a70244f66208bbb8920
shyqulreyad <shyqulislam@gmail.com>
2023-05-21 14:05:42 +0600

    scalar and verctor tensor

--- pyTorch/intro.py
+++ pyTorch/intro.py
@@ -4,3 +4,24 @@ pytorch is a python package that provides two high-level features:
     2. Deep Neural Networks built on a tape-based autograd system
     
 '''
+
+import torch
+
+# print(torch.__version__)
+
+# Tensor
+# A tensor is a generalization of vectors and matrices to potentially higher dimensions.
+# Internally, pytorch represents tensors as n-dimensional arrays of base datatypes.
+# A tensor can be constructed from a Python list or sequence using the torch.tensor() constructor.
+
+#Scalar
+x = torch.tensor(3.14159)
+print(x)
+print(type(x))
+print(x.ndim)
+print(x.shape)
+print(x.item())
+
+#Vector
+vactor = torch.tensor([1,2,3,4,5])
+print(vactor)
