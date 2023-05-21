6c13ada1aef2009c6cff54589c21551de9554c44
shyqulreyad <shyqulislam@gmail.com>
2023-05-21 14:12:53 +0600

    tensor

--- pyTorch/intro.py
+++ pyTorch/intro.py
@@ -15,13 +15,31 @@ import torch
 # A tensor can be constructed from a Python list or sequence using the torch.tensor() constructor.
 
 #Scalar
+'''
 x = torch.tensor(3.14159)
 print(x)
 print(type(x))
 print(x.ndim)
 print(x.shape)
 print(x.item())
+'''
 
 #Vector
+'''
 vactor = torch.tensor([1,2,3,4,5])
 print(vactor)
+print(vactor.ndim)
+print(vactor.shape)
+'''
+#Matrix
+matrix = torch.tensor([[1,2,3],[4,5,6]])
+print(matrix)
+print(matrix.ndim)
+print(matrix.shape)
+print(matrix[0][0])
+
+#Tensor
+tensor = torch.tensor([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(tensor)
+print(tensor.ndim)
+print(tensor.shape)
