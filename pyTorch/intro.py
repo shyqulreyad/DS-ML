3d3775ebde9bf454e29c41c82fe1ca1a0ab835af
shyqulreyad <shyqulislam@gmail.com>
2023-05-21 13:46:48 +0600

    pytorch intro

---
+++ pyTorch/intro.py
@@ -0,0 +1,6 @@
+'''
+pytorch is a python package that provides two high-level features:
+    1. Tensor computation (like numpy) with strong GPU acceleration
+    2. Deep Neural Networks built on a tape-based autograd system
+    
+'''
